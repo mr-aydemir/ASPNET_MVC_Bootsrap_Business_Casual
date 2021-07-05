5994106159a6eddfbe581b9c42a09494f3aa73db
mr-aydemir <aydemir_emre65@hotmail.com>
2021-07-05 22:24:19 +0300

    HreflerDüzeltildi

--- StaticWebSiteMVC/Controllers/HomeController.cs
+++ StaticWebSiteMVC/Controllers/HomeController.cs
@@ -13,14 +13,17 @@ namespace StaticWebSiteMVC.Controllers
         {
             return View();
         }
+        [Route("hakkimizda")]
         public ActionResult About()
         {
             return View();
         }
+        [Route("urunler")]
         public ActionResult Products()
         {
             return View();
         }
+        [Route("magaza")]
         public ActionResult Store()
         {
             return View();
